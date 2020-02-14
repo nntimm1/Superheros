aaf815f1b0774074429bcf08e1e1b02343b93818
Nicolas Timm <nntimm1@gmail.com>
2020-02-14 14:35:39 -0600

    initial push

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Superheros.Models;
+
+namespace Superheros.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            DbSet<Superhero> Superheros;
+        }
+    }
+}
---
+++ Models/Superhero.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Superheros.Models
+{
+    public class Superhero
+    {
+        [Key]
+        public int SuperheroID;
+        public string Name;
+        public string AlterEgo;
+        public string PrimaryAbility;
+        public string SecondaryAbility;
+
+    }
+}
